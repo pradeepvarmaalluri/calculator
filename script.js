2aba167c5c3eb78a8458b22d130e6c626d60590a
pradeepvarmaalluri <163242470+pradeepvarmaalluri@users.noreply.github.com>
2025-03-08 16:48:29 +0530

    script

---
+++ script.js
@@ -0,0 +1,32 @@
+let displayInput=document.getElementById("calculationInput");
+let previousValue;
+function appendValue(value)
+{
+    
+        displayInput.value+=value;
+        
+   
+}
+function clearDisplay()
+{
+    
+    previousValue=eval(displayInput.value);
+    
+    document.getElementById("presult").textContent = previousValue;
+    displayInput.value="";
+}
+function clearElement(){
+    displayInput.value=displayInput.value.slice(0,-1);
+}
+function calculate()
+{
+    try{
+        let a=eval(displayInput.value);
+        
+        displayInput.value=a;
+        
+       
+    }catch{
+        document.getElementById("result").innerText = "Error..";
+    }
+}
